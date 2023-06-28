039d7d851baa5c667ef122853c98db90889c0c05
Gustavo Lima <gustavo.f12@hotmail.com>
2023-06-28 14:04:35 -0300

    SignUp logic working.

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+    Handler(Looper.getMainLooper()).postDelayed({
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+        finish()
+    },3000)
+
     }
 }
--- app/src/main/java/com/example/login/SignUpActivity.kt
+++ app/src/main/java/com/example/login/SignUpActivity.kt
@@ -2,10 +2,17 @@ package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
 import com.example.login.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +20,63 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
+        binding.buttonSignUp.setOnClickListener {
+
+            val email = binding.editTextEmail.text.toString()
+            val password = binding.editTextPassword.text.toString()
+            if (checkAllFields()) {
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        auth.signOut()
+                        Toast.makeText(this,"Account created successfully!",Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        Log.e("error: ", it.exception.toString())
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun checkAllFields(): Boolean {
+
+        val email = binding.editTextEmail.text.toString()
+
+        if (binding.editTextEmail.text.toString() == "") {
+            binding.textInputLayoutEmail.error = "This is required field"
+            return false
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.textInputLayoutEmail.error = "Check email format"
+            return false
+        }
+        if (binding.editTextPassword.text.toString() == "") {
+            binding.textInputLayoutPasssword.error = "This is required field"
+            binding.textInputLayoutPasssword.errorIconDrawable = null
+            return false
+        }
+
+        if (binding.editTextPassword.text.toString().length <= 7){
+            binding.textInputLayoutPasssword.error = "Password should be at least 8 characters long "
+            binding.textInputLayoutPasssword.errorIconDrawable = null
+            return false
+        }
+
+        if (binding.editTextConfirmPassword.text.toString() == "") {
+            binding.textInputLayoutConfirmPasssword.error = "This is required field"
+            binding.textInputLayoutConfirmPasssword.errorIconDrawable = null
+            return false
+        }
+
+        if (binding.editTextPassword.text.toString() != binding.editTextConfirmPassword.text.toString()) {
+            binding.textInputLayoutPasssword.error = "Password do not match"
+            return false
+        }
 
 
+        return true
     }
 }
